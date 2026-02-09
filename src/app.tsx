c71b44b239bb1df54f789f4499cec81966dcd5bd
izume01 <shrey16211@gmail.com>
2026-02-09 12:16:38 +0530

    feat(normalize) : added a normalize function for different type of values

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,10 @@ import { Rizzy } from "./runtime";
 /** @jsx Rizzy.createElement */
 const element = (
   <div>
-    <h1>Hello, world!</h1>
+    <h1 style={{
+      color: "red",
+      fontSize: "24px"
+    }}>Hello, world!</h1>
     <p>This is a simple example of a React-like element.</p>
   </div>
 );
